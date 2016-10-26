337dd2ab860625190a89eb0e2365d1fe36648aac
Drake Ballard <drakeballard@gmail.com>
2016-10-25 19:30:03 -0500

    adding the comments to the js file

--- http7000_7500.js
+++ http7000_7500.js
@@ -1,9 +1,11 @@
+// npm package
 var http = require ('http');
 
+//declar two variables for the port
 var GOODPORT = 7000;
 var BADPORT = 7500;
 
-//IMPORTANT
+//2 functions that handle either for good and bad
 function handleRequest1(request, response) {
   response.end('Looking Good BRO: ' + request.url);
 }
@@ -12,16 +14,14 @@ function handleRequest2(request, response) {
   response.end('Looking shitty bro: ' + request.url);
 }
 
+//create servers from the npm packages
 var gserver = http.createServer(handleRequest1);
-
-
 var bserver = http.createServer(handleRequest2);
 
-
+//start spinning the servers
 gserver.listen(GOODPORT, function() {
   console.log("This Server is listneing on: http://localhost:%s", GOODPORT);
 });
-
 
 bserver.listen(BADPORT, function() {
   console.log("This Server is listneing on: http://localhost:%s", BADPORT);
